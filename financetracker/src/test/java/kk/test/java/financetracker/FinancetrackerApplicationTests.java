fe33e42e3daec603a6e4bf1c71ef6c81ed8ea999
Karthick K <134149445+Karthick454@users.noreply.github.com>
2024-06-01 23:41:24 +0530

    Initial Commit after Spring initiliazer

---
+++ financetracker/src/test/java/kk/test/java/financetracker/FinancetrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package kk.test.java.financetracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinancetrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
